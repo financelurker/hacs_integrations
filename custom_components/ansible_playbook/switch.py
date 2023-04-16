03204d826f3b1742c6b7750c14c2e10975f7c804
financelurker <financelurker_github@blunzn.club>
2023-04-16 12:17:22 +0200

    Switch to ansible_runner module for PlaybookExecutor

--- custom_components/ansible_playbook/switch.py
+++ custom_components/ansible_playbook/switch.py
@@ -4,15 +4,13 @@ import json
 from datetime import datetime
 from typing import List
 
-from ansible import context
 from ansible.inventory.manager import InventoryManager
 from ansible.parsing.dataloader import DataLoader
 from ansible.playbook.play import Play
 from ansible.plugins.callback import CallbackBase
 from ansible.plugins.loader import callback_loader
 from ansible.vars.manager import VariableManager
-from ansible.executor.playbook_executor import PlaybookExecutor
-from ansible.module_utils.common.collections import ImmutableDict
+from ansible_runner.runner import PlaybookExecutor
 
 import time
 import homeassistant.helpers.config_validation as cv
@@ -205,12 +203,10 @@ class AnsiblePlaybookSwitch(SwitchEntity):
         results_callback = PlaybookResultCallback()
         results_callback.playbook_path = self._playbook_path
 
-        #options = {
-        #    'extra_vars': self._extra_vars,
-        #    'vault_password_file': self._vault_password_file
-        #}
-
-        context.CLIARGS = ImmutableDict(vault_password_file=self._vault_password_file)
+        options = {
+            'extra_vars': self._extra_vars,
+            'vault_password_file': self._vault_password_file
+        }
 
         # Set up playbook
         playbook = PlaybookExecutor(
@@ -219,7 +215,7 @@ class AnsiblePlaybookSwitch(SwitchEntity):
             variable_manager=variable_manager,
             loader=loader,
             passwords={},
-        #    options=options,
+            options=options,
             callbacks=[results_callback],
             stdout_callback=self._callback,
         )
