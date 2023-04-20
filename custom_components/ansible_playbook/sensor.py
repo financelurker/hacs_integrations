5692da63d13b1bd6c46c79adb5b5438d217b1eca
financelurker <financelurker_github@blunzn.club>
2023-04-20 08:19:12 +0200

    Simplify _update_with_state in sensor

--- custom_components/ansible_playbook/sensor.py
+++ custom_components/ansible_playbook/sensor.py
@@ -71,17 +71,14 @@ class AnsiblePlaybookSensorEntity(SensorEntity):
         _LOGGER.debug("AnsiblePlaybookSensorEntity.async_update exit")
 
     def _update_with_state(self, task_state: AnsibleTaskState):
-        if task_state == None:
-            _LOGGER.debug("AnsiblePlaybookSensorEntity._update_with_state enter - the state is None")
-        else:
-            _LOGGER.debug("AnsiblePlaybookSensorEntity._update_with_state enter - the state is " + task_state.name)
-            if task_state == AnsibleTaskState.NOT_RUNNING and self._state == True:
-                _LOGGER.debug("AnsiblePlaybookSensorEntity._update_with_state playbook NOT_RUNNING: turning off sensor")
-                result = collect_result(self._button_unique_id)
-                _LOGGER.debug(result)
-                self._state = False
-                self.async_write_ha_state()
-            _LOGGER.debug("AnsiblePlaybookSensorEntity._update_with_state exit")
+        _LOGGER.debug("AnsiblePlaybookSensorEntity._update_with_state enter - the state is " + task_state.name)
+        if task_state == AnsibleTaskState.NOT_RUNNING and self._state == True:
+            _LOGGER.debug("AnsiblePlaybookSensorEntity._update_with_state playbook NOT_RUNNING: turning off sensor")
+            result = collect_result(self._button_unique_id)
+            _LOGGER.debug(result)
+            self._state = False
+            self.async_write_ha_state()
+        _LOGGER.debug("AnsiblePlaybookSensorEntity._update_with_state exit")
 
 class AnsiblePlaybookHostExecutionResultSensorEntity(SensorEntity):
     def __init__(self):
